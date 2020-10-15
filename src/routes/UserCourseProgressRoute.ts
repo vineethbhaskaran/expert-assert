363aee91ccc1dbdfe7dfbdd31e573e2d379c75bb
vineeth bhaskaran <vineethbhaskaran@outlook.com>
2020-10-15 15:34:44 +0530

    added query string instead of params

--- src/routes/UserCourseProgressRoute.ts
+++ src/routes/UserCourseProgressRoute.ts
@@ -4,16 +4,14 @@ import UserCourseProgressService from "../service/UserCourseProgressService";
 
 const routes = Router();
 
-export const getCourseProgress = routes.get(
-  "/courseProgress/:courseId",
-  async (request: Request, response: Response) => {
-    const courseId = request.params.courseId;
-    try {
-      const userProgress = await UserCourseProgressService.loadfirstPage(courseId);
-      return response.json(userProgress);
-    } catch (errorResponse) {
-      logger.logMessage(errorResponse);
-      return response.json(errorResponse);
-    }
+export const getCourseProgress = routes.get("/courseProgress", async (request: Request, response: Response) => {
+  const courseId = request.query.courseId;
+  try {
+    //@ts-ignore
+    const userProgress = await UserCourseProgressService.loadfirstPage(courseId);
+    return response.json(userProgress);
+  } catch (errorResponse) {
+    logger.logMessage(errorResponse);
+    return response.json(errorResponse);
   }
-);
+});
