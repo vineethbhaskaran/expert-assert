e186f4e9f06d7197246bcdef082381811bf07081
vineeth bhaskaran <vineethbhaskaran@outlook.com>
2020-10-23 19:21:00 +0530

    added hook methods for executing test runner

--- test/testCourse.ts
+++ test/testCourse.ts
@@ -7,35 +7,62 @@ import { app } from "../src/index";
 const should = chai.should();
 
 chai.use(chaiHttp);
+describe("hooks", function () {
+  before(function () {
+    // runs once before the first test in this block
+  });
 
-describe("/Get all courses", () => {
-  it("The status should be 200", (done) => {
-    chai
-      .request(app)
-      .get("/courses")
-      .end((err, result) => {
-        result.should.have.status(200);
-        result.should.be.an("object");
-        done();
-      });
+  after(function () {
+    // runs once after the first test in this block
   });
-  it("result should be an object", (done) => {
-    chai
-      .request(app)
-      .get("/courses")
-      .end((err, result) => {
-        result.should.be.an("object");
-        done();
-      });
+
+  beforeEach(function () {
+    // runs before each test in this block
   });
-  it("result should exist", (done) => {
-    chai
-      .request(app)
-      .get("/courses")
-      .end((err, result) => {
-        console.log(JSON.stringify(result));
-        should.exist(result);
-        done();
-      });
+
+  afterEach(function () {
+    // runs after each test in this block
+  });
+
+  describe("/Get all courses", () => {
+    it("The status should be 200", (done) => {
+      chai
+        .request(app)
+        .get("/courses")
+        .end((err, result) => {
+          result.should.have.status(200);
+          result.should.be.an("object");
+          done();
+        });
+    });
+    it("result should be an object", (done) => {
+      chai
+        .request(app)
+        .get("/courses")
+        .end((err, result) => {
+          result.should.be.an("object");
+          done();
+        });
+    });
+    it("result should exist", (done) => {
+      chai
+        .request(app)
+        .get("/courses")
+        .end((err, result) => {
+          console.log(JSON.stringify(result));
+          should.exist(result);
+          done();
+        });
+    });
+    it("result should have property data which is an array w", (done) => {
+      chai
+        .request(app)
+        .get("/courses")
+        .end((err, result) => {
+          console.log(JSON.stringify(result.body));
+          result.body.should.have.property("data").which.is.an("array");
+          done();
+        });
+    });
   });
 });
